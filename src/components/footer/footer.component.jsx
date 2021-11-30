1860ef34bc97583657d1ac5e28a014b7332f84e7
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-11-30 11:08:48 +0100

    Fixed the height of the Footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -6,7 +6,7 @@ import "./footer.styles.scss";
 
 const Footer = () => {
   return (
-    <footer className="relative bg-gray-200 dark:bg-gray-800 pt-8 pb-6 mb-0">
+    <footer className="relative bg-gray-200 dark:bg-gray-800 pt-8 pb-6 mb-0 mt-10">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
